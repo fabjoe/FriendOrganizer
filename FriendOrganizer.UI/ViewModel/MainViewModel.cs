0b43a2818dbd35daccb014290ab0fb11540e1e46
fabrizioc <fabrizioc@silversea.com>
2017-12-01 17:13:05 +0100

    Add project files.

---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+using System.Collections.ObjectModel;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel
+    {
+        private readonly IFriendDataService _friendDataService;
+        private Friend _selectedFriend;
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            _friendDataService = friendDataService;
+            Friends = new ObservableCollection<Friend>();
+        }
+
+        public void Load()
+        {
+            var friends = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach(var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        
+
+        public Friend SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set { _selectedFriend = value; }
+        }
+    }
+}
